2f950b09da521764d9a717ec3483a57b663029e0
AgrMayank <myid.mayank@gmail.com>
2019-06-08 17:26:56 +0530

    Files added.

---
+++ Assets/Scripts/PlayerAnimController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerAnimController : MonoBehaviour
+{
+
+	private Animator anim;
+	public NavMeshAgent navMeshAgent;
+	public bool isWalking;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		anim = GetComponent<Animator> ();
+		navMeshAgent = GetComponent<NavMeshAgent> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		/*
+		if (navMeshAgent.velocity != Vector3.zero)
+		{
+			isWalking = true;
+		}
+		else
+		{
+			isWalking = false;
+		}
+
+		anim.SetBool ("IsWalking", isWalking);
+		*/
+
+		anim.SetFloat ("PlayerVel", navMeshAgent.velocity.magnitude);
+
+		print (navMeshAgent.velocity.magnitude);
+    }
+}
